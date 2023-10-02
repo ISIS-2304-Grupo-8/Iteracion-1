36493499a220f41ffe1def3eb61c6841d6e882f0
Pandlop <jc.lopezc1@uniandes.edu.co>
2023-10-01 19:58:08 -0500

    jc controller listos no avanzados

---
+++ src/main/java/uniandes/edu/co/proyecto/controllers/IncluyenController.java
@@ -0,0 +1,51 @@
+package uniandes.edu.co.proyecto.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import uniandes.edu.co.proyecto.modelo.IncluyenEntity;
+import uniandes.edu.co.proyecto.modelo.IncluyenEntityPK;
+import uniandes.edu.co.proyecto.modelo.PlanDeConsumoEntity;
+import uniandes.edu.co.proyecto.modelo.ProductoEntity;
+import uniandes.edu.co.proyecto.repositories.IncluyenRepository;
+import uniandes.edu.co.proyecto.repositories.PlanDeConsumoRepository;
+import uniandes.edu.co.proyecto.repositories.ProductoRepository;
+
+@Controller
+public class IncluyenController {
+    @Autowired
+    private IncluyenRepository incluyenRepository;
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Autowired
+    private PlanDeConsumoRepository planDeConsumoRepository;
+
+    @GetMapping("/incluyen/new")
+    public String incluyenForm(Model model) {
+        model.addAttribute("productos", productoRepository.darProductos());
+        model.addAttribute("planes", planDeConsumoRepository.darPlanesDeConsumo());
+        return "incluyenNew";
+    }
+
+    @PostMapping("/incluyen/new/save")
+    public String incluyenSave(@ModelAttribute("prod_id") Integer id_producto,
+            @ModelAttribute("tipo_servicio") String tipo_servicio, @ModelAttribute("id_plan") Integer id_plan) {
+
+        ProductoEntity producto = productoRepository.darProducto(id_producto, tipo_servicio);
+        PlanDeConsumoEntity plan = planDeConsumoRepository.darPlanDeConsumo(id_plan);
+        IncluyenEntityPK pk = new IncluyenEntityPK(plan, producto);
+        IncluyenEntity incluyen = new IncluyenEntity();
+        incluyen.setPk(pk);
+
+        incluyenRepository.crearIncluyen(incluyen.getPk().getProducto().getPk().getId_producto(),
+                incluyen.getPk().getPlanesdeconsumo_id_plan().getId_plan(),
+                incluyen.getPk().getProducto().getPk().getTipos_servicio_tipo_servicio().getTipo_servicio());
+        return "redirect:/planesDeConsumo";
+    }
+}
--- src/main/java/uniandes/edu/co/proyecto/repositories/IncluyenRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositories/IncluyenRepository.java
@@ -11,26 +11,29 @@ import org.springframework.transaction.annotation.Transactional;
 import uniandes.edu.co.proyecto.modelo.IncluyenEntity;
 
 public interface IncluyenRepository extends JpaRepository<IncluyenEntity, Integer> {
-    // Read incluyen
-    @Query(value = "SELECT * FROM incluyen", nativeQuery = true)
-    Collection<IncluyenEntity> darIncluyen();
+        // Read incluyen
+        @Query(value = "SELECT * FROM incluyen", nativeQuery = true)
+        Collection<IncluyenEntity> darIncluyen();
 
-    // Read incluyen dado un producto y un plan de consumo
-    @Query(value = "SELECT * FROM incluyen WHERE prod_id =:prod_id AND pdc_id=:pdc_id AND prod_id_ser=:prod_id_ser", nativeQuery = true)
-    IncluyenEntity darIncluyen(@Param("prod_id") Integer prod_id, @Param("pdc_id") Integer pdc_id,
-            @Param("prod_id_ser") Integer prod_id_ser);
+        // Read incluyen dado un producto y un plan de consumo
+        @Query(value = "SELECT * FROM incluyen WHERE planesdeconsumo_id_plan =:planesdeconsumo_id_plan AND productos_id_producto=:productos_id_producto AND productos_tipo_servicio=:productos_tipo_servicio", nativeQuery = true)
+        IncluyenEntity darIncluyen(@Param("planesdeconsumo_id_plan") Integer prod_id,
+                        @Param("productos_id_producto") Integer pdc_id,
+                        @Param("productos_tipo_servicio") String tipo_servicio);
 
-    // Crear un incluyen
-    @Modifying
-    @Transactional
-    @Query(value = "INSERT INTO incluyen (prod_id, pdc_id, prod_id_ser) VALUES (:prod_id, :pdc_id, :prod_id_ser)", nativeQuery = true)
-    void crearIncluyen(@Param("prod_id") Integer prod_id, @Param("pdc_id") Integer pdc_id,
-            @Param("prod_id_ser") Integer prod_id_ser);
+        // Crear un incluyen
+        @Modifying
+        @Transactional
+        @Query(value = "INSERT INTO incluyen (planesdeconsumo_id_plan, productos_id_producto, productos_tipo_servicio) VALUES (:planesdeconsumo_id_plan, :productos_id_producto, :productos_tipo_servicio)", nativeQuery = true)
+        void crearIncluyen(@Param("productos_id_producto") Integer prod_id,
+                        @Param("planesdeconsumo_id_plan") Integer pdc_id,
+                        @Param("productos_tipo_servicio") String tipo_servicio);
 
-    // Eliminar incluyen
-    @Modifying
-    @Transactional
-    @Query(value = "DELETE FROM incluyen WHERE prod_id =:prod_id AND pdc_id=:pdc_id AND prod_id_ser=:prod_id_ser", nativeQuery = true)
-    void eliminarIncluyen(@Param("prod_id") Integer prod_id, @Param("pdc_id") Integer pdc_id,
-            @Param("prod_id_ser") Integer prod_id_ser);
+        // Eliminar incluyen
+        @Modifying
+        @Transactional
+        @Query(value = "DELETE FROM incluyen WHERE productos_id_producto =:productos_id_producto AND planesdeconsumo_id_plan=:planesdeconsumo_id_plan AND productos_tipo_servicio=:productos_tipo_servicio", nativeQuery = true)
+        void eliminarIncluyen(@Param("productos_id_producto") Integer prod_id,
+                        @Param("planesdeconsumo_id_plan") Integer pdc_id,
+                        @Param("productos_tipo_servicio") String tipo_servicio);
 }
