006ca31f7987ee2330e35fe85e1e97ef34b3fb09
Pandlop <jc.lopezc1@uniandes.edu.co>
2023-10-30 15:01:50 -0500

    r7 paginacion

--- src/main/java/uniandes/edu/co/proyecto/controllers/HabitacionesController.java
+++ src/main/java/uniandes/edu/co/proyecto/controllers/HabitacionesController.java
@@ -20,25 +20,22 @@ import uniandes.edu.co.proyecto.repositories.HabitacionRepository;
 import uniandes.edu.co.proyecto.repositories.HabitacionRepository.RespuestaReq1;
 import uniandes.edu.co.proyecto.repositories.HabitacionRepository.RespuestaOcupacion;
 
-
 @Controller
 public class HabitacionesController {
 
     @Autowired
     private HabitacionRepository habitacionRepository;
 
-
     @GetMapping("/habitaciones")
-    public ResponseEntity<Page<HabitacionEntity>> habitaciones(Pageable pageable){
-    Page<HabitacionEntity> habPage = habitacionRepository.findAll(pageable);
-    return new ResponseEntity<>(habPage, HttpStatus.OK);
+    public ResponseEntity<Page<HabitacionEntity>> habitaciones(Pageable pageable) {
+        Page<HabitacionEntity> habPage = habitacionRepository.findAll(pageable);
+        return new ResponseEntity<>(habPage, HttpStatus.OK);
     }
 
-
-   @GetMapping("/habitaciones/{id_habitacion}")
-    public String habitacion(Model model, @PathVariable("id_habitacion") Integer id_habitacion){
+    @GetMapping("/habitaciones/{id_habitacion}")
+    public String habitacion(Model model, @PathVariable("id_habitacion") Integer id_habitacion) {
         HabitacionEntity habitacion = habitacionRepository.darHabitacion(id_habitacion);
-        if(habitacion != null){
+        if (habitacion != null) {
             model.addAttribute("habitacion", habitacion);
             return "habitacion";
         } else {
@@ -46,23 +43,23 @@ public class HabitacionesController {
         }
     }
 
-
     @GetMapping("/habitaciones/new")
-    public String habitacionForm(Model model){
+    public String habitacionForm(Model model) {
         model.addAttribute("habitacion", new HabitacionEntity());
         return "habitacion_new";
     }
 
     @PostMapping("/habitaciones/new/save")
-    public String habitacionGuardar(@ModelAttribute HabitacionEntity habitacion){
-        habitacionRepository.insertarHabitacion(habitacion.getConsumo_acumulado(), habitacion.getDisponibilidad(), habitacion.getTipos_habitacion_id_tipo().getId_tipo());
+    public String habitacionGuardar(@ModelAttribute HabitacionEntity habitacion) {
+        habitacionRepository.insertarHabitacion(habitacion.getConsumo_acumulado(), habitacion.getDisponibilidad(),
+                habitacion.getTipos_habitacion_id_tipo().getId_tipo());
         return "redirect:/habitaciones";
     }
 
     @GetMapping("/habitaciones/{id_habitacion}/edit")
-    public String habitacionEditar(Model model, @PathVariable("id_habitacion") Integer id_habitacion){
+    public String habitacionEditar(Model model, @PathVariable("id_habitacion") Integer id_habitacion) {
         HabitacionEntity habitacion = habitacionRepository.darHabitacion(id_habitacion);
-        if(habitacion != null){
+        if (habitacion != null) {
             model.addAttribute("habitacion", habitacion);
             return "habitacion_edit";
         } else {
@@ -71,36 +68,42 @@ public class HabitacionesController {
     }
 
     @PostMapping("/habitaciones/{id_habitacion}/edit/save")
-    public String habitacionEditarGuardar(@PathVariable("id_habitacion") Integer id_habitacion, @ModelAttribute HabitacionEntity habitacion){
-        habitacionRepository.actualizarHabitacion(id_habitacion, habitacion.getConsumo_acumulado(), habitacion.getDisponibilidad(), habitacion.getTipos_habitacion_id_tipo().getId_tipo());
+    public String habitacionEditarGuardar(@PathVariable("id_habitacion") Integer id_habitacion,
+            @ModelAttribute HabitacionEntity habitacion) {
+        habitacionRepository.actualizarHabitacion(id_habitacion, habitacion.getConsumo_acumulado(),
+                habitacion.getDisponibilidad(), habitacion.getTipos_habitacion_id_tipo().getId_tipo());
         return "redirect:/habitaciones";
     }
 
     @GetMapping("/habitaciones/{id_habitacion}/delete")
-    public String habitacionEliminar(@PathVariable("id_habitacion") Integer id_habitacion){
+    public String habitacionEliminar(@PathVariable("id_habitacion") Integer id_habitacion) {
         habitacionRepository.eliminarHabitacion(id_habitacion);
         return "redirect:/habitaciones";
     }
 
     // @GetMapping("/habitaciones/dinero_recolectado")
     // public String habitacionDineroRecolectado(Model model){
-    //     Collection<RespuestaReq1> informacion = habitacionRepository.darDineroRecolectadoServiciosPorHabitacion();
-    //     model.addAttribute("habitaciones", informacion.iterator().next().getID_HABITACION());
-    //     model.addAttribute("dinero_recolectado", informacion.iterator().next().getDINERO_RECOLECTADO());
-    //     return "habitaciones_dinero_recolectado";
+    // Collection<RespuestaReq1> informacion =
+    // habitacionRepository.darDineroRecolectadoServiciosPorHabitacion();
+    // model.addAttribute("habitaciones",
+    // informacion.iterator().next().getID_HABITACION());
+    // model.addAttribute("dinero_recolectado",
+    // informacion.iterator().next().getDINERO_RECOLECTADO());
+    // return "habitaciones_dinero_recolectado";
     // }
 
     @GetMapping("/habitaciones/dinero_recolectado")
-    public ResponseEntity<Collection<RespuestaReq1>> habitacionesDinero(Pageable pageable){
-    Collection<RespuestaReq1> habPage = habitacionRepository.darDineroRecolectadoServiciosPorHabitacion();
-    return new ResponseEntity<>(habPage, HttpStatus.OK);
+    public ResponseEntity<Collection<RespuestaReq1>> habitacionesDinero(Pageable pageable) {
+        Collection<RespuestaReq1> habPage = habitacionRepository.darDineroRecolectadoServiciosPorHabitacion();
+        return new ResponseEntity<>(habPage, HttpStatus.OK);
     }
 
     @GetMapping("/habitaciones/indice_ocupacion")
-    public ResponseEntity<Collection<RespuestaOcupacion>> obtenerIndiceOcupacion(@RequestParam(required = false) Integer size, @RequestParam(required = false) Integer offset) {
-        Collection<RespuestaOcupacion> resultado = habitacionRepository.obtenerIndiceOcupacionPorHabitacion(size, offset);
-    return new ResponseEntity<>(resultado, HttpStatus.OK);
+    public ResponseEntity<Collection<RespuestaOcupacion>> obtenerIndiceOcupacion(
+            @RequestParam(required = false) Integer size, @RequestParam(required = false) Integer offset) {
+        Collection<RespuestaOcupacion> resultado = habitacionRepository.obtenerIndiceOcupacionPorHabitacion(size,
+                offset);
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
     }
 
-    
 }
--- src/main/resources/static/connection_gerente.js
+++ src/main/resources/static/connection_gerente.js
@@ -1,4 +1,33 @@
-let currentPage = 0; // Página actual. Empieza en 0.    
+let currentPage = 0; // Página actual. Empieza en 0.  
+let size = 5; // Cantidad de habitaciones a mostrar por página.
+
+
+/**
+ * Función para actualizar la cantidad de registros por página y recargar los datos.
+ * @param {Event} event - Evento del cambio en el selector de registros por página.
+ */
+function updateRecordsPerPage(event) {
+    size = parseInt(event.target.value);
+    currentPage = 0; // Reiniciar la paginación.
+    fetchAndDisplayClients(currentPage, size);
+}
+
+/**
+ * Función para habilitar o deshabilitar los botones de paginación.
+ * @param {Object} data - Datos de paginación del backend.
+ */
+function handlePaginationButtons(data) {
+    const prevBtnLi = document.querySelector('.page-link[href="#prev"]').parentNode;
+    const nextBtnLi = document.querySelector('.page-link[href="#next"]').parentNode;
+
+    // Controlar la visibilidad de los botones de paginación.
+    if (data.first) prevBtnLi.classList.add('disabled');
+    else prevBtnLi.classList.remove('disabled');
+
+    if (data.last) nextBtnLi.classList.add('disabled');
+    else nextBtnLi.classList.remove('disabled');
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector('.btn.btn-secondary.goback').addEventListener('click', function() {
@@ -6,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-function fetchAndDisplayServices(page = 0, size = 10) {
+function fetchAndDisplayServices(page, size) {
     const f_in = document.getElementById('f_in').value;
     const f_fin = document.getElementById('f_fin').value;
 
@@ -113,105 +142,131 @@ function fetchAndDisplayServices(page = 0, size = 10) {
         });
 }
 
- function fetchAndDisplayClients(page=0, size=10){
-    fetch(`/clientes/buenos_clientes?page=${page}&size=${size}`)
+
+//BUENOS CLIENTES
+function displayGoodClients(clientes){
+    const container = document.querySelector('.card-body.modRF7');
+
+    // Generar contenido para la tabla.
+    let tableContent = `
+        <table class="table table-striped table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">ID Cliente</th>
+                    <th scope="col">Días Estadía</th>
+                    <th scope="col">Consumo Total</th>
+                </tr>
+            </thead>
+            <tbody>
+    `;
+
+
+    clientes.forEach((cli) => {
+        if (cli.dias_ESTADIA == null) cli.dias_ESTADIA = '-';
+        if (cli.consumo_TOTAL == null) cli.consumo_TOTAL = '-';
+        tableContent += `
+            <tr>
+                <td>${cli.cedula_CLIENTE}</td>
+                <td>${cli.dias_ESTADIA}</td>
+                <td>${cli.consumo_TOTAL}</td>
+            </tr>
+        `;
+    });
+
+    tableContent += `</tbody></table>`;
+    container.innerHTML = tableContent;
+}
+
+function fetchAndDisplayClients(page, size){
+    fetch(`/clientes/buenos_clientes?size=${size}&offset=${page}`)
     .then(response => response.json())
     .then(data => {
-        const clientsTableBody = document.getElementById('goodClientsTableBody');
-        clientsTableBody.innerHTML = ''; // Limpiar el contenido anterior
-
-        data.forEach((cli) => {
-            if (cli.dias_ESTADIA == null) cli.dias_ESTADIA = '-';
-            if (cli.consumo_TOTAL == null) cli.consumo_TOTAL = '-';
-            const cliRow = `
-                <tr>
-                    <td>${cli.cedula_CLIENTE}</td>
-                    <td>${cli.dias_ESTADIA}</td>
-                    <td>${cli.consumo_TOTAL}</td>
-                </tr>
-            `;
-            clientsTableBody.insertAdjacentHTML('beforeend', cliRow);
-        });
-
-        // Lógica de paginación
-        const paginationControls = document.getElementById('paginationControlsGoodClients');
-        const totalPages = data.totalPages;
-
-        // Limpiar controles de paginación existentes
-        paginationControls.innerHTML = `
-            <li class="page-item"><a class="page-link" href="#" id="previousPage">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#" id="nextPage">Next</a></li>
-        `;
-
-        // Insertar números de página
-        for (let i = 0; i < totalPages; i++) {
-            const pageItem = document.createElement('li');
-            pageItem.className = 'page-item';
-            pageItem.innerHTML = `<a class="page-link" href="#" data-page="${i}">${i + 1}</a>`;
-            paginationControls.insertBefore(pageItem, document.getElementById('nextPage').parentNode);
-        }
-
-        // Deshabilitar los botones anterior/siguiente según sea necesario
-        document.getElementById('previousPage').parentNode.classList.toggle('disabled', page === 0);
-        document.getElementById('nextPage').parentNode.classList.toggle('disabled', page === totalPages - 1);
-
-        currentPage = page; // Actualizar página actual
+        displayGoodClients(data);
+        handlePaginationButtons(data);
     })
     .catch(error => {
         console.error('Error fetching users:', error);
     });
 }
 
+function nextPage() {
+    currentPage += size;
+    fetchAndDisplayClients(currentPage, size);
+}
+
+function prevPage() {
+    currentPage = Math.max(0, currentPage - size); // No permitir páginas negativas.
+    fetchAndDisplayClients(currentPage, size);
+}
+
+
+
+
+
 
 
 // Agregar controladores de eventos para los botones de paginación
 document.addEventListener("DOMContentLoaded", function() {
+
+    document.querySelector('.btn.btn-secondary.goback').addEventListener('click', () => {
+        window.location.href = "/";
+    });
+
+    // Cargar los datos iniciales.
+    fetchAndDisplayClients(currentPage, size);
+
+    // Asignar eventos a los botones de paginación.
+    document.querySelector('.page-link[href="#prev"]').addEventListener("click", prevPage);
+    document.querySelector('.page-link[href="#next"]').addEventListener("click", nextPage);
+
+    // Asignar evento al selector de cantidad de registros por página.
+    document.getElementById('recordsPerPage').addEventListener('change', updateRecordsPerPage);
     
-    document.getElementById('paginationControls').addEventListener('click', function(event) {
-        if (event.target.tagName === 'A' && !event.target.parentNode.classList.contains('disabled')) {
-            const clickedPage = event.target.getAttribute('data-page');
+    // document.getElementById('paginationControls').addEventListener('click', function(event) {
+    //     if (event.target.tagName === 'A' && !event.target.parentNode.classList.contains('disabled')) {
+    //         const clickedPage = event.target.getAttribute('data-page');
 
-            if (clickedPage !== null) {
-                fetchAndDisplayMoney(parseInt(clickedPage));
-            } else if (event.target.id === 'previousPage') {
-                fetchAndDisplayMoney(currentPage - 1);
-            } else if (event.target.id === 'nextPage') {
-                fetchAndDisplayMoney(currentPage + 1);
-            }
+    //         if (clickedPage !== null) {
+    //             fetchAndDisplayMoney(parseInt(clickedPage));
+    //         } else if (event.target.id === 'previousPage') {
+    //             fetchAndDisplayMoney(currentPage - 1);
+    //         } else if (event.target.id === 'nextPage') {
+    //             fetchAndDisplayMoney(currentPage + 1);
+    //         }
 
-            event.preventDefault();
-        }
-    });
+    //         event.preventDefault();
+    //     }
+    // });
 
-    document.getElementById('paginationControlsServices').addEventListener('click', function(event) {
-        if (event.target.tagName === 'A' && !event.target.parentNode.classList.contains('disabled')) {
-            const clickedPage = event.target.getAttribute('data-page');
+    // document.getElementById('paginationControlsServices').addEventListener('click', function(event) {
+    //     if (event.target.tagName === 'A' && !event.target.parentNode.classList.contains('disabled')) {
+    //         const clickedPage = event.target.getAttribute('data-page');
 
-            if (clickedPage !== null) {
-                fetchAndDisplayServices(parseInt(clickedPage));
-            } else if (event.target.id === 'previousPage') {
-                fetchAndDisplayServices(currentPage - 1);
-            } else if (event.target.id === 'nextPage') {
-                fetchAndDisplayServices(currentPage + 1);
-            }
+    //         if (clickedPage !== null) {
+    //             fetchAndDisplayServices(parseInt(clickedPage));
+    //         } else if (event.target.id === 'previousPage') {
+    //             fetchAndDisplayServices(currentPage - 1);
+    //         } else if (event.target.id === 'nextPage') {
+    //             fetchAndDisplayServices(currentPage + 1);
+    //         }
 
-            event.preventDefault();
-        }
-    });
+    //         event.preventDefault();
+    //     }
+    // });
 
-    document.getElementById('paginationControlsGoodClients').addEventListener('click', function(event) {
-        if (event.target.tagName === 'A' && !event.target.parentNode.classList.contains('disabled')) {
-            const clickedPage = event.target.getAttribute('data-page');
+    // document.getElementById('paginationControlsGoodClients').addEventListener('click', function(event) {
+    //     if (event.target.tagName === 'A' && !event.target.parentNode.classList.contains('disabled')) {
+    //         const clickedPage = event.target.getAttribute('data-page');
 
-            if (clickedPage !== null) {
-                fetchAndDisplayClients(parseInt(clickedPage));
-            } else if (event.target.id === 'previousPage') {
-                fetchAndDisplayClients(currentPage - 1);
-            } else if (event.target.id === 'nextPage') {
-                fetchAndDisplayClients(currentPage + 1);
-            }
+    //         if (clickedPage !== null) {
+    //             fetchAndDisplayClients(parseInt(clickedPage));
+    //         } else if (event.target.id === 'previousPage') {
+    //             fetchAndDisplayClients(currentPage - 1);
+    //         } else if (event.target.id === 'nextPage') {
+    //             fetchAndDisplayClients(currentPage + 1);
+    //         }
 
-            event.preventDefault();
-        }
-    });
+    //         event.preventDefault();
+    //     }
+    // });
 }); 
